6279ea0d782cb14d9f322a30bbe25894f543dc81
Jakub Kasprzyk <neus90@gmail.com>
2016-01-17 17:13:13 +0100

    build static html files

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ var fs = require('fs');
 
 var app = express();
 
+var distPath = 'dist/';
+var pages = [
+  {
+    fileName: 'index.html',
+    pageName: 'homepage'
+  }, {
+    fileName: 'regulamin.html',
+    pageName: 'rules'
+  }
+];
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -14,19 +25,37 @@ app.use('/', express.static('public'));
 // homepage
 app.get('/', function(req, res) {
   res.render('layout', {
-    content: getPageContent('homepage')
+    content: getPageContent('pages/homepage')
   });
 });
 
 app.get('/regulamin', function(req, res) {
   res.render('layout', {
-    content: getPageContent('rules')
+    content: getPageContent('pages/rules')
   });
+});
+
+app.get('/build', function(req, res) {
+  pages.forEach(function (page) {
+    var fileName = distPath + page.fileName;
+    var stream = fs.createWriteStream(fileName);
+
+    stream.once('open', function(fd) {
+      var html = buildHtml(page.pageName);
+      stream.end(html);
+    });
+  });
+
+  res.send('build succeeded');
 });
 
 app.listen(8080);
 console.log('server is running on localhost:8080');
 
 function getPageContent(fileName) {
-  return ejs.render(fs.readFileSync('views/pages/' + fileName + '.ejs', 'utf-8'));
+  return ejs.render(fs.readFileSync('views/' + fileName + '.ejs', 'utf-8'));
+}
+
+function buildHtml(pageName) {
+  return getPageContent('partials/head') + getPageContent('pages/' + pageName) + getPageContent('partials/footer');
 }
