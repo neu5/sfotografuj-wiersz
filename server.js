2ca9ba9154303e0404afc3dfbd84ea5eca0236b1
Jakub Kasprzyk <neus90@gmail.com>
2016-01-19 19:08:25 +0100

    move `pages` to the external file

--- server.js
+++ server.js
@@ -3,30 +3,13 @@
 var express = require('express');
 var ejs = require('ejs');
 var fs = require('fs');
-var path = require("path");
+var path = require('path');
+
+var Pages = require('./pages');
 
 var app = express();
 
 var distPath = 'dist';
-var pages = [
-  {
-    fileName: 'index.html',
-    pageName: 'homepage.ejs',
-    uri: '/'
-  }, {
-    fileName: 'regulamin.html',
-    pageName: 'rules.ejs',
-    uri: '/regulamin'
-  }, {
-    fileName: 'o-konkursie.html',
-    pageName: 'about.ejs',
-    uri: '/o-konkursie'
-  }, {
-    fileName: 'zglos-sie.html',
-    pageName: 'apply.ejs',
-    uri: '/zglos-sie'
-  }
-];
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -34,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use('/', express.static('public'));
 
 // loop for default uris
-pages.forEach(page => {
+Pages.pages.forEach(page => {
   app.get(page.uri, (req, res) => {
     res.render('layout', {
       content: getPageContent(path.join('pages', page.pageName))
